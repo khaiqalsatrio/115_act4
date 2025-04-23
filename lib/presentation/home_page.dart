ea8fbdc51dcef940fad326d9aec9bf8134f21d72
Khaiqol <Khaiqol@gmail.com>
2025-04-23 20:23:41 +0700

    memebuat home state page

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -2,7 +2,33 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Selamat datang di home page'),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OrderPage()),
+                );
+              },
+              child: const Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
